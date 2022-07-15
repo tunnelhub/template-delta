445f8a12bf91c5b5aaf128a86442ad8fa4869922
Renan William Alves de Paula <renan@4success.com.br>
2022-07-15 09:55:01 -0300

    Adding comments for support

--- src/classes/integration.ts
+++ src/classes/integration.ts
@@ -4,67 +4,114 @@ import { GenericParameter, TunnelHubSystem } from '@4success/tunnelhub-sdk/src/t
 import { IntegrationModel } from '../data';
 
 export default class Integration extends DeltaIntegrationFlow {
-  private static keyFields: string[] = ['key_field'];
-  private static deltaFields: string[] = ['regular_field'];
+  private static keyFields: string[] = [
+    'key_field',
+  ];
+  private static deltaFields: string[] = [
+    'regular_field',
+  ];
 
   private readonly parameters: { custom: GenericParameter[] };
   private readonly systems: TunnelHubSystem[];
 
   constructor(event: any, context: any) {
     super(event, Integration.keyFields, Integration.deltaFields, context);
-
     this.systems = event.systems ?? [];
     this.parameters = event.parameters ?? {};
+    /**
+     * It is mandatory have the constructor and call the super with event of main handler
+     * You can get the systems configured in automation and save in class property for further use
+     */
   }
 
   /* istanbul ignore next */
-  defineMetadata = (): Metadata[] => [
-    {
-      fieldName: 'key_field',
-      fieldLabel: 'Key field',
-      fieldType: 'TEXT'
-    },
-    {
-      fieldName: 'regular_field',
-      fieldLabel: 'Regular field',
-      fieldType: 'TEXT'
-    }
-  ];
+  defineMetadata(): Metadata[] {
+    /**
+     * Return all columns that will be visible in monitoring screen.
+     * The components order is the display order in monitoring table
+     *
+     * The implementation of this method is mandatory
+     */
+    return [
+      {
+        fieldName: 'key_field',
+        fieldLabel: 'Key field',
+        fieldType: 'TEXT',
+      },
+      {
+        fieldName: 'regular_field',
+        fieldLabel: 'Regular field',
+        fieldType: 'TEXT',
+      },
+    ];
+  }
 
-  loadSourceSystemData = async (payload?: any): Promise<IntegrationModel[]> => [
-    {
-      key_field: '1',
-      regular_field: 'anyString'
-    },
-    {
-      key_field: '2',
-      regular_field: 'anotherString'
-    }
-  ];
+  async loadSourceSystemData(payload?: any): Promise<IntegrationModel[]> {
+    /**
+     * Return the source system data as a plain array of objets
+     *
+     * This is the method where you will extract your source data
+     * If your automation is a webhook, the payload sent  will be avaiable in "payload" parameter.
+     *
+     * The implementation of this method is mandatory
+     */
+    return [
+      {
+        key_field: '1',
+        regular_field: 'anyString',
+      },
+      {
+        key_field: '2',
+        regular_field: 'anotherString',
+      },
+    ];
+  }
 
-  loadTargetSystemData = async (): Promise<IntegrationModel[]> => [
-    {
-      key_field: '2',
-      regular_field: 'oldAnotherString'
-    },
-    {
-      key_field: '3',
-      regular_field: 'anyotherstring'
-    }
-  ];
+  async loadTargetSystemData(): Promise<IntegrationModel[]> {
+    /**
+     * Return target source system data as a plain array of objets
+     * This is the method where you will extract your target data, for delta calculation
+     */
+    return [
+      {
+        key_field: '2',
+        regular_field: 'oldAnotherString',
+      },
+      {
+        key_field: '3',
+        regular_field: 'anyotherstring',
+      },
+    ];
+  }
 
-  insertAction = async (item: IntegrationModel): Promise<IntegrationMessageReturn> => ({
-    data: {},
-    message: 'Inserted successfully'
-  });
+  async insertAction(item: IntegrationModel): Promise<IntegrationMessageReturn> {
+    /**
+     * Create new items in the target system and return a message to moniroting
+     */
+    return ({
+      data: {},
+      message: 'Inserted successfully',
+    });
+  }
 
-  updateAction = async (oldItem: IntegrationModel, newItem: IntegrationModel): Promise<IntegrationMessageReturn> => ({
-    data: {},
-    message: 'Updated successfully'
-  });
+  async updateAction(oldItem: IntegrationModel, newItem: IntegrationModel): Promise<IntegrationMessageReturn> {
+    return {
+      /**
+       * Update existing items in the target system and return a message to moniroting
+       */
+      data: {},
+      message: 'Updated successfully',
+    };
+  }
 
-  deleteAction = async (item: IntegrationModel): Promise<IntegrationMessageReturn> => ({
-    data: {},
-    message: 'Delete successfully'
-  });
+  async deleteAction(item: IntegrationModel): Promise<IntegrationMessageReturn> {
+    /**
+     * Delete existing items in the target system not extracted on source system and return a message to moniroting
+     */
+    return ({
+      data: {},
+      message: 'Delete successfully',
+    });
+  }
+
 }
